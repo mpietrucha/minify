4d21108bba3a9b766eb09456ae306ea0d0be6433
Michał Pietrucha <michal.pietrucha@off-site.pl>
2023-03-28 13:30:23 +0200

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,15 @@
 <?php
+
+use Mpietrucha\Minify\Minify;
+
+if (! function_exists('minify')) {
+    function minify(?string $contents = null, array $options = []): Minify|string {
+        $handler = new Minify;
+
+        if (! $contents) {
+            return $handler
+        }
+
+        return $handler->content($contents)->options($options)->minify();
+    }
+}
